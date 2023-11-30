a8aa17585dd14a93a65c1a72aec0ab23f652c183
moeezkashif13 <moeezkashif13@gmail.com>
2023-11-30 15:04:17 +0500

    working

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,16 @@
-import { Reviews } from '@/components/ClientComponents/HomepageClient'
-import Footer from '@/components/Footer'
-import HeroSect from '@/components/HeroSect'
-import ThreeDivs from '@/components/ThreeDivs'
-
-
-
-
+import { Reviews } from "@/components/Client/Homepage";
+import Footer from "@/components/Footer";
+import HeroSect from "@/components/HeroSect";
+import ThreeDivs from "@/components/ThreeDivs";
 
 export default function Home() {
   return (
-
-
     <div>
+      <HeroSect />
 
+      <ThreeDivs />
 
-<HeroSect/>
-
-
-<ThreeDivs/>
-
-
-<Reviews/>
-
-
+      <Reviews />
     </div>
-    
-
-
-
-
-  )
+  );
 }
--- src/components/HeroSect.jsx
+++ src/components/HeroSect.jsx
@@ -1,67 +1,45 @@
-import { HeroSectText, LogosSlider } from './ClientComponents/HomepageClient';
+import { HeroSectText, LogosSlider } from "./Client/Homepage";
 
+export default function HeroSect() {
+  //
+  return (
+    <div className="bg-neutral-700">
+      <HeroSectText />
 
+      <div className="flex border-t border-b border-neutral-600 ">
+        {[1, 2].map(() => {
+          return (
+            <div
+              style={{ transition: "all 0.3s" }}
+              className="hover:bg-neutral-600 p-6  w-1/2 first:border-r first:border-r-neutral-600  space-y-6  sm:space-y-12"
+            >
+              <div>
+                <p className="text-[#f0f0f0] font-medium text-lg ">
+                  Repositories
+                </p>
 
-export default function HeroSect(){
+                <p className="text-[#f0f0f0]">
+                  Build production-ready projects with your team.
+                </p>
+              </div>
 
-    
+              <div className="flex justify-between text-lime-600 items-center leading-5">
+                <p>Import repository</p>
 
-// 
-return <div className='bg-neutral-700' >
+                <p>ic</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
 
+      {/* Slider */}
 
-<HeroSectText/>
+      <div className="  slider hideScrollbar overflow-scroll  h-[90px] ">
+        <LogosSlider />
+      </div>
 
-<div className='flex border-t border-b border-neutral-600 '>
-
-
-{[1,2].map(()=>{
-    return <div style={{transition:'all 0.3s'}} className='hover:bg-neutral-600 p-6  w-1/2 first:border-r first:border-r-neutral-600  space-y-6  sm:space-y-12'>
-
-<div>
-
-<p className='text-[#f0f0f0] font-medium text-lg '>Repositories</p>
-
-<p className='text-[#f0f0f0]'>Build production-ready projects with your team.</p>
-
-</div>
-
-<div className='flex justify-between text-lime-600 items-center leading-5'>
-
-
-<p>Import repository</p>
-
-<p>ic</p>
-
-</div>
-
-
+      {/* Slider */}
     </div>
-})}
-
-
-
-
-</div>
-
-
-
-
-{/* Slider */}
-
-
-<div className='  slider hideScrollbar overflow-scroll  h-[90px] '>
-
-<LogosSlider/>
-
-</div>
-
-
-{/* Slider */}
-
-
-
-
-
-    </div>
+  );
 }
